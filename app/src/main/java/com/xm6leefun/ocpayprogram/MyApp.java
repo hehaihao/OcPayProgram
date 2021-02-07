871f14f3898f426071a788c500d5c887701cab54
hehaihao <7703619+hehaiao@user.noreply.gitee.com>
2021-02-07 17:35:11 +0800

    初始化

--- app/src/main/java/com/xm6leefun/ocpayprogram/MyApp.java
+++ app/src/main/java/com/xm6leefun/ocpayprogram/MyApp.java
@@ -3,16 +3,23 @@ package com.xm6leefun.ocpayprogram;
 import android.app.Application;
 
 import com.onlychain.signsdk.wallet.base.ApiConfig;
+import com.xm6leefun.ocpayprogram.utils.ShowUtil;
+
+import androidx.multidex.MultiDex;
+import androidx.multidex.MultiDexApplication;
 
 /**
  * @Description:
  * @Author: hhh
  * @CreateDate: 2021/2/7 9:37
  */
-public class MyApp extends Application {
+public class MyApp extends MultiDexApplication {
     @Override
     public void onCreate() {
         super.onCreate();
+        MultiDex.install(this);
+        //初始化
+        ShowUtil.initialize(this);
         ApiConfig.init("http://39.98.135.66:9082");
     }
 }
